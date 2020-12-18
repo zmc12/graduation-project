ebb8c7e2d55bf15d281a87445ac3906577653825
zhangmincong <1040958446@qq.com>
2020-12-18 11:12:25 +0800

    init

---
+++ src/main/java/com/jsut/JSUTApplication.java
@@ -0,0 +1,15 @@
+package com.jsut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZhangMinCong
+ * @date 2020/12/18 10:57
+ */
+@SpringBootApplication
+public class JSUTApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JSUTApplication.class,args);
+    }
+}
