fcf2de24b8c8e9c443119b39553f2f6055646f88
zhangmincong <1040958446@qq.com>
2021-01-19 17:07:50 +0800

    前端更新

---
+++ src/main/java/com/jsut/web/controller/userController.java
@@ -0,0 +1,33 @@
+package com.jsut.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author ZhangMinCong
+ * @date 2021/1/19 16:10
+ */
+
+@Controller
+@RequestMapping("/user")
+public class userController {
+
+    @PostMapping("/login")
+    public String login(@RequestParam("user")String user,@RequestParam("password")String password){
+       if(("admin").equals(user) && ("12345").equals(password)){
+           return "nav";
+       }else {
+           return "index";
+       }
+
+    }
+
+    @GetMapping("/logout")
+    public String loginout(){
+        return "index";
+    }
+
+}
