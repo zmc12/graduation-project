bdbaeeb829fba4ed0ab4522c03446bae5993034a
zhangmincong <1040958446@qq.com>
2021-01-21 16:21:13 +0800

    后端更新

--- src/main/java/com/jsut/web/controller/UserController.java
+++ src/main/java/com/jsut/web/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.jsut.web.controller;
 
+import com.jsut.web.pojo.Teacher;
+import com.jsut.web.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +21,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Slf4j
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/login")
-    public String login(@RequestParam("user")String user,@RequestParam("password")String password){
-       if(("admin").equals(user) && ("12345").equals(password)){
-           return "home";
-       }else {
+    public String login(@RequestParam("user")String user, @RequestParam("password")String password){
+
+        Teacher teacher=userService.selectByUser(user,password);
+
+       if("".equals(teacher) || teacher==null){
            return "index";
+       }else {
+           return "home";
        }
 
     }
